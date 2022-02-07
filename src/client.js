d3863772e5403bb4dacd66bf18e94ce6dd3bcb22
Noah <tmddnjs7231@gmail.com>
2022-02-08 00:07:53 +0900

    make it better

--- src/client.js
+++ src/client.js
@@ -1,4 +1,6 @@
-import { PrismaClient } from '@prisma/client'
+/* import { PrismaClient } from '@prisma/client' */
+
+import { PrismaClient } from '@prisma/client';
 
 const client = new PrismaClient()
 
